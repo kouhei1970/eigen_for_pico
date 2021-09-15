1a578c644e68dbec969373aaace5fe6b894f7f71
Kouhei <kouhei.ito@itolab-ktc.com>
2021-09-16 06:06:58 +0900

    Add files via upload

---
+++ test_eigen.cpp
@@ -0,0 +1,29 @@
+/**
+ Copyright (c) 2021 Kouhei Ito 
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include "hardware/irq.h"
+#include <Eigen/Dense>
+
+using Eigen::MatrixXd;
+                
+int main() {
+    stdio_init_all();
+
+    while (1){
+        tight_loop_contents();
+        MatrixXd m(2,2);
+        m(0,0) = 3;
+        m(1,0) = 2.5;
+        m(0,1) = -1;
+        m(1,1) = m(1,0) + m(0,1);
+        std::cout << m << std::endl;       
+        printf("Hello!\n");
+    }
+}
+
+/// \end:uart_advanced[]
