b1ce47be5107f9c2a0799f04908219f8b0cedc99
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2021-09-19 05:16:40 +0900

    update code

--- test_eigen.cpp
+++ test_eigen.cpp
@@ -1,5 +1,5 @@
-/**
- Copyright (c) 2021 Kouhei Ito 
+/*
+  Copyright (c) 2021 Kouhei Ito 
 */
 
 #include <stdio.h>
@@ -8,20 +8,29 @@
 #include <Eigen/Dense>
 
 using Eigen::MatrixXd;
-                
+
 int main() {
-    stdio_init_all();
+  char dummy;
+  short i;
+  stdio_init_all();
 
-    while (1){
-        tight_loop_contents();
-        MatrixXd m(2,2);
-        m(0,0) = 3;
-        m(1,0) = 2.5;
-        m(0,1) = -1;
-        m(1,1) = m(1,0) + m(0,1);
-        std::cout << m << std::endl;       
-        printf("Hello!\n");
-    }
+  for(i=0; i<30;i++){
+    printf("Please wait for %d second.\n",30-i);
+    sleep_ms(1000);
+  }
+  printf("Let's start calculation.\n\n");
+
+  i=0;
+  while (1){
+    tight_loop_contents();
+    printf("Hit return key! %d\n",i++);
+    scanf("%c",&dummy);
+
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
+  }
 }
-
-/// \end:uart_advanced[]
